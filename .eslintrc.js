b9b4544376a73f0d88c26d44da70ffc0262652a9
Ivan G <mr.zero@inbox.ru>
2023-06-22 22:05:59 +0300

    build: setup webpack, ts, eslint

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    ignorePatterns: ['webpack.config.js'],
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        'import/extensions': 'off',
+        'import/prefer-default-export': 'off',
+        '@typescript-eslint/no-explicit-any': 'error',
+        '@typescript-eslint/array-type': [
+          'error',
+          {
+            default: 'array',
+          },
+        ],
+        '@typescript-eslint/explicit-member-accessibility': [
+          'error',
+          {
+            accessibility: 'explicit',
+            overrides: {
+              accessors: 'explicit',
+              constructors: 'off',
+              methods: 'explicit',
+              properties: 'explicit',
+              parameterProperties: 'explicit',
+            },
+          },
+        ],
+        'max-lines-per-function': ['error', 40],
+        '@typescript-eslint/explicit-function-return-type': 'error',
+        '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+        '@typescript-eslint/no-non-null-assertion': 'error',
+        'no-console': 'off',
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const EslintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/i,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new EslintPlugin({ extensions: 'ts' }),
+  ],
+};
