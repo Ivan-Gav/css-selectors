aafefc5fbb90a0c1591266578b478d7edcdfec5e
Ivan G <mr.zero@inbox.ru>
2023-07-03 17:20:33 +0300

    feat: add reset progress function

--- src/widgets/levels-list/levels-list.ts
+++ src/widgets/levels-list/levels-list.ts
@@ -2,6 +2,7 @@
 import levelsListTemplate from "../../entities/templates/levels-list-template";
 import PageElement from "../../shared/page-element";
 import levels from "../../entities/levels/levels";
+import Button from "../../shared/button";
 
 const levelsListParms = {
   element: "div",
@@ -18,6 +19,50 @@ export default class LevelsListPanel extends PageElement {
     completedLevels: Set<number>,
     currentLevel: number
   ): HTMLElement {
+    
+    const listItemClickHandler = (e: Event): void => {
+      const burger = document.querySelector(".burger");
+      const list = document.querySelector(".open");
+      const targ = e.currentTarget;
+      if (burger instanceof HTMLElement) {
+        burger.dispatchEvent(new Event("click"));
+        if (list instanceof HTMLElement) {
+          list.ontransitionend = (): void => {
+            if (
+              targ instanceof HTMLElement &&
+              "dataset" in targ &&
+              "nr" in targ.dataset
+            ) {
+              const nr = Number(targ.dataset.nr);
+              document.dispatchEvent(
+                new CustomEvent("changeLevel", {
+                  detail: { win: false, lvl: nr },
+                })
+              );
+            }
+          };
+        }
+      }
+    };
+
+    const resetProgressHandler = (): void => {
+      const burger = document.querySelector(".burger");
+      const list = document.querySelector(".open");
+      if (burger instanceof HTMLElement) {
+        burger.dispatchEvent(new Event("click"));
+        if (list instanceof HTMLElement) {
+          list.ontransitionend = (): void => {
+            document.dispatchEvent(new CustomEvent("resetProgress"));
+            document.dispatchEvent(
+              new CustomEvent("changeLevel", {
+                detail: { win: false, lvl: 0 },
+              })
+            );
+          };
+        }
+      }
+    };
+    
     const levelsList = document.createElement("div");
     levelsList.className = "levels-list";
     levels.forEach((level, i) => {
@@ -35,35 +80,19 @@ export default class LevelsListPanel extends PageElement {
       item.innerHTML += `<span class="level-number">${i + 1}</span>`;
       item.innerHTML += `${level.syntax}`;
 
-      const listItemClickHandler = (e: Event): void => {
-        const burger = document.querySelector(".burger");
-        const list = document.querySelector(".open");
-        const targ = e.currentTarget;
-        if (burger instanceof HTMLElement) {
-          burger.dispatchEvent(new Event("click"));
-          if (list instanceof HTMLElement) {
-          list.ontransitionend = ():void => {
-            if (
-              targ instanceof HTMLElement &&
-              "dataset" in targ &&
-              "nr" in targ.dataset
-            ) {
-              const nr = Number(targ.dataset.nr);
-              document.dispatchEvent(
-                new CustomEvent("changeLevel", {
-                  detail: { win: false, lvl: nr },
-                })
-              );
-            }
-          };
-        }
-      }
-      };
       item.addEventListener("click", listItemClickHandler);
       levelsList.append(item);
     });
+
+    const reset = new Button(
+      "ResetProgress",
+      "reset-button",
+      resetProgressHandler
+    );
+    const resetButton = reset.getButton();
+
     const levelsListPan = this.build();
-    levelsListPan.append(levelsList);
+    levelsListPan.append(levelsList, resetButton);
 
     return levelsListPan;
   }
